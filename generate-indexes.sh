a686fd851ab4b02d81e659b8282f0a1f01faf53f
jowens25 <jowens@novuspower.com>
2026-02-10 16:36:15 -0600

    index

--- generate-indexes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# generate-indexes.sh
-find ~/.aptly/public -type d | while read dir; do
-    (cd "$dir" && ls -1p | awk 'BEGIN{print "<html><body><ul>"} {print "<li><a href=\""$1"\">"$1"</a></li>"} END{print "</ul></body></html>"}' > index.html)
-done

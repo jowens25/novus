37985b6e901460662911f9f6c2d9938ff674960d
jowens25 <jowens@novuspower.com>
2026-02-18 11:29:29 -0600

    updated install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 wget -qO- https://jowens25.github.io/novus/public-key.asc | sudo gpg --dearmor -o /etc/apt/keyrings/novus.gpg
 echo "deb [signed-by=/etc/apt/keyrings/novus.gpg] https://jowens25.github.io/novus stable main" | sudo tee /etc/apt/sources.list.d/novus.list
 sudo apt update
-sudo apt install ns-agent ns-serial-mux ns2
+sudo apt install ns-agent ns-serial-mux ns2 -y
